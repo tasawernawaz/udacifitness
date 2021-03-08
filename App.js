aa5c51280dd1b0d00f72325096fd3f1d6e8880e9
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-03-08 16:56:32 +0500

    basic setup for udaciy classroom project

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>new message</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
